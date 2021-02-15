6e66308561c27933913ea166080713b3d6c4f4b1
javier <johnnybelesaca@gmail.com>
2021-02-15 14:45:43 -0500

    Feature: Working Calculator with All Functions

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,5 +1,37 @@
+from src.calculator.functions.Addition import addition
+from src.calculator.functions.Subtraction import subtraction
+from src.calculator.functions.Division import division
+from src.calculator.functions.Multiply import multiply
+from src.calculator.functions.Square import square
+from src.calculator.functions.SquareRoot import square_root
+
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
+
+    def add(self, x, y):
+        self.result = addition(x, y)
+        return self.result
+
+    def subtract(self, x, y):
+        self.result = subtraction(x, y)
+        return self.result
+
+    def multiply(self, x, y):
+        self.result = multiply(x, y)
+        return self.result
+
+    def divide(self, x, y):
+        self.result = division(x, y)
+        return self.result
+
+    def square(self, x):
+        self.result = square(x)
+        return self.result
+
+    def square_root(self, x):
+        self.result = square_root(x)
+        return self.result
