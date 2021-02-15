d1153905917fde737f72ae5eb85b6ea3679602d7
javier <johnnybelesaca@gmail.com>
2021-02-15 14:46:38 -0500

    Fix: Testing Calculator Functions

--- tests/test_Calculator.py
+++ tests/test_Calculator.py
@@ -13,5 +13,29 @@ class CalculatorTestCase(unittest.TestCase):
     def test_result_is_zero_calculator(self):
         self.assertEqual(self.calculator.result, 0)
 
+    def test_subtraction(self):
+        self.assertEqual(self.calculator.subtract(5, 6), 1)
+
+    def test_addition(self):
+        self.assertEqual(self.calculator.add(5, 6), 11)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calculator.multiply(5, 6), 30)
+
+    def test_division(self):
+        self.assertEqual(self.calculator.divide(6, 30), 5)
+
+    def test_square(self):
+        self.assertEqual(self.calculator.square(5), 25)
+
+    def test_square_root(self):
+        self.assertEqual(self.calculator.square_root(100), 10)
+        '''
+        test_data = CsvReader("Tests/Data/subtraction.csv").data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, result)
+        '''
 
 
