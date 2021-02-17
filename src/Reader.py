fdb945817ce8e1a2b382665768cbcf9b5a8ae8f1
javier <johnnybelesaca@gmail.com>
2021-02-17 00:50:13 -0500

    Task: made absolute path

--- src/Reader.py
+++ src/Reader.py
@@ -1,4 +1,10 @@
 import csv
+from pathlib import Path
+
+
+def absolute_path(filepath):
+    relative = Path(filepath)
+    return relative.absolute()
 
 
 class Reader:
@@ -9,7 +15,7 @@ class Reader:
         self.reader(file)
 
     def reader(self, path):
-        with open(path) as csv_file:
+        with open(absolute_path(path)) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             first_line = csv_file.readline()  # to get rid of the first line i.e val1, val2, result
             for row in csv_reader:
