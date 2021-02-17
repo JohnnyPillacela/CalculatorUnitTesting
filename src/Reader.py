6d7252ebc9be0d76c8238c6be5d9f5b67bf49e63
javier <johnnybelesaca@gmail.com>
2021-02-17 00:40:55 -0500

    Task: made a class for Reader.py instead of just 1 functions

--- src/Reader.py
+++ src/Reader.py
@@ -1,14 +1,20 @@
 import csv
 
-content = []
 
+class Reader:
+    content = []
 
-def reader(file):
-    with open(file) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        first_line = csv_file.readline()  # to get rid of the first line i.e val1, val2, result
-        for row in csv_reader:
-            if '' in row:
-                row.remove('')
-            content.append(row)
-    return content
+    def __init__(self, file):
+        self.content = []
+        self.reader(file)
+
+    def reader(self, path):
+        with open(path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            first_line = csv_file.readline()  # to get rid of the first line i.e val1, val2, result
+            for row in csv_reader:
+                if '' in row:
+                    row.remove('')
+                self.content.append(row)
+        #csv_file.close()
+        #return self.content
